595106e8ba0ae1aef5ebaca999a127805cc42434
Yiğitcan UÇUM <3743507+Yengas@users.noreply.github.com>
2023-08-03 23:31:23 +0300

    feat: refactor ui related code to different module

--- tui/src/main.rs
+++ tui/src/main.rs
@@ -4,20 +4,23 @@ use std::{
 };
 
 use crossterm::{
-    event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode, KeyEventKind},
+    event::{DisableMouseCapture, EnableMouseCapture, Event, EventStream, KeyCode, KeyEventKind},
     execute,
     terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
 };
-use ratatui::{prelude::*, widgets::*};
+use ratatui::prelude::*;
 use state::{App, InputMode};
+use tokio_stream::StreamExt;
 
 mod state;
+mod ui;
 
-fn main() -> anyhow::Result<()> {
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
     let app = App::default();
     let mut terminal = setup_terminal()?;
 
-    run(&mut terminal, app)?;
+    run(&mut terminal, app).await?;
 
     restore_terminal(&mut terminal)?;
     Ok(())
@@ -40,12 +43,19 @@ fn restore_terminal(terminal: &mut Terminal<CrosstermBackend<Stdout>>) -> anyhow
     Ok(terminal.show_cursor()?)
 }
 
-fn run(terminal: &mut Terminal<CrosstermBackend<Stdout>>, mut app: App) -> anyhow::Result<()> {
-    Ok(loop {
-        terminal.draw(|frame| ui(frame, &mut app))?;
+async fn run(
+    terminal: &mut Terminal<CrosstermBackend<Stdout>>,
+    mut app: App,
+) -> anyhow::Result<()> {
+    let mut ticker = tokio::time::interval(Duration::from_millis(250));
+    let mut crossterm_events = EventStream::new();
 
-        if event::poll(Duration::from_millis(250))? {
-            if let Event::Key(key) = event::read()? {
+    loop {
+        tokio::select! {
+            // Tick to terminate the select every N milliseconds
+            _ = ticker.tick() => (),
+            // Catch and handle crossterm events
+            Some(Ok(Event::Key(key))) = crossterm_events.next() => {
                 match app.input_mode {
                     InputMode::Normal => match key.code {
                         KeyCode::Char('e') => {
@@ -77,155 +87,10 @@ fn run(terminal: &mut Terminal<CrosstermBackend<Stdout>>, mut app: App) -> anyho
                     },
                     _ => {}
                 }
+
             }
         }
-    })
-}
 
-fn ui<B: Backend>(frame: &mut Frame<B>, app: &App) {
-    let [left, middle, right] = *Layout::default()
-        .direction(Direction::Horizontal)
-        .constraints(
-            [
-                Constraint::Percentage(20),
-                Constraint::Percentage(60),
-                Constraint::Percentage(20),
-            ]
-            .as_ref(),
-        )
-        .split(frame.size()) else {
-            panic!("The main layout should have 3 chunks")
-        };
-
-    let [container_room_list, container_user_info] = *Layout::default()
-        .direction(Direction::Vertical)
-        .constraints(
-            [
-                Constraint::Min(1),
-                Constraint::Length(10),
-            ]
-            .as_ref(),
-        )
-        .split(left) else {
-            panic!("The left layout should have 2 chunks")
-        };
-
-    let room_list: Vec<ListItem> = vec!["#test", "#room1", "#room2"]
-        .iter()
-        .map(|room_name| {
-            let content = Line::from(Span::raw(format!("{room_name}")));
-
-            ListItem::new(content)
-        })
-        .collect();
-    let room_list =
-        List::new(room_list).block(Block::default().borders(Borders::ALL).title("Rooms"));
-    frame.render_widget(room_list, container_room_list);
-
-    let user_info = Paragraph::new("User: @jjohndoejohndoejohndoejohndoejohndoeohndoe").block(
-        Block::default()
-            .borders(Borders::ALL)
-            .title("User Information"),
-    );
-    frame.render_widget(user_info, container_user_info);
-
-    let [container_highlight, container_messages, container_input] = *Layout::default()
-        .direction(Direction::Vertical)
-        .constraints(
-            [
-                Constraint::Length(3),
-                Constraint::Min(1),
-                Constraint::Length(3),
-            ]
-            .as_ref(),
-        )
-        .split(middle) else {
-            panic!("The middle layout should have 3 chunks")
-        };
-
-    let text = Text::from(Line::from(vec![
-        "on ".into(),
-        "#room1".bold(),
-        " for ".into(),
-        "interesting talks about life".italic(),
-    ]));
-    let help_message = Paragraph::new(text).block(
-        Block::default()
-            .borders(Borders::ALL)
-            .title("Active Room Information"),
-    );
-    frame.render_widget(help_message, container_highlight);
-
-    let messages: Vec<ListItem> = app
-        .messages
-        .iter()
-        .enumerate()
-        .map(|(i, m)| {
-            let content = Line::from(Span::raw(format!("{i}: {m}")));
-            ListItem::new(content)
-        })
-        .collect();
-    let messages =
-        List::new(messages).block(Block::default().borders(Borders::ALL).title("Messages"));
-    frame.render_widget(messages, container_messages);
-
-    let input = Paragraph::new(app.input.as_str())
-        .style(match app.input_mode {
-            InputMode::Normal => Style::default(),
-            InputMode::Editing => Style::default().fg(Color::Yellow),
-        })
-        .block(Block::default().borders(Borders::ALL).title("Input"));
-    frame.render_widget(input, container_input);
-    match app.input_mode {
-        InputMode::Normal =>
-            // Hide the cursor. `Frame` does this by default, so we don't need to do anything here
-            {}
-
-        InputMode::Editing => {
-            // Make the cursor visible and ask ratatui to put it at the specified coordinates after
-            // rendering
-            frame.set_cursor(
-                // Draw the cursor at the current position in the input field.
-                // This position is can be controlled via the left and right arrow key
-                container_input.x + app.cursor_position as u16 + 1,
-                // Move one line down, from the border to the input line
-                container_input.y + 1,
-            )
-        }
+        terminal.draw(|frame| ui::render_app_too_frame(frame, &mut app))?;
     }
-
-    let [container_room_users, container_usage] = *Layout::default()
-        .direction(Direction::Vertical)
-        .constraints(
-            [
-                Constraint::Min(1),
-                Constraint::Length(10),
-            ]
-            .as_ref(),
-        )
-        .split(right) else {
-            panic!("The left layout should have 2 chunks")
-        };
-
-    let room_users_list_items: Vec<ListItem> = vec!["jjohndoe", "jane", "john"]
-        .iter()
-        .map(|user_name| {
-            let content = Line::from(Span::raw(format!("@{user_name}")));
-
-            ListItem::new(content)
-        })
-        .collect();
-    let room_users_list = List::new(room_users_list_items)
-        .block(Block::default().borders(Borders::ALL).title("Room Users"));
-
-    frame.render_widget(room_users_list, container_room_users);
-
-    let mut usage_text = Text::from(vec![
-        Line::from(vec!["(q)".bold(), " to exit".into()]),
-        Line::from(vec!["(e)".bold(), " to start editing".into()]),
-    ]);
-    usage_text.patch_style(Style::default().add_modifier(Modifier::RAPID_BLINK));
-    let usage =
-        Paragraph::new(usage_text).block(Block::default().borders(Borders::ALL).title("Usage"));
-    frame.render_widget(usage, container_usage);
 }
---
+++ tui/src/ui.rs
@@ -0,0 +1,151 @@
+use ratatui::{prelude::*, widgets::*};
+
+use crate::state::{App, InputMode};
+
+pub(crate) fn render_app_too_frame<B: Backend>(frame: &mut Frame<B>, app: &App) {
+    let [left, middle, right] = *Layout::default()
+        .direction(Direction::Horizontal)
+        .constraints(
+            [
+                Constraint::Percentage(20),
+                Constraint::Percentage(60),
+                Constraint::Percentage(20),
+            ]
+            .as_ref(),
+        )
+        .split(frame.size()) else {
+            panic!("The main layout should have 3 chunks")
+        };
+
+    let [container_room_list, container_user_info] = *Layout::default()
+        .direction(Direction::Vertical)
+        .constraints(
+            [
+                Constraint::Min(1),
+                Constraint::Length(10),
+            ]
+            .as_ref(),
+        )
+        .split(left) else {
+            panic!("The left layout should have 2 chunks")
+        };
+
+    let room_list: Vec<ListItem> = vec!["#test", "#room1", "#room2"]
+        .iter()
+        .map(|room_name| {
+            let content = Line::from(Span::raw(format!("{room_name}")));
+
+            ListItem::new(content)
+        })
+        .collect();
+    let room_list =
+        List::new(room_list).block(Block::default().borders(Borders::ALL).title("Rooms"));
+    frame.render_widget(room_list, container_room_list);
+
+    let user_info = Paragraph::new("User: @jjohndoejohndoejohndoejohndoejohndoeohndoe").block(
+        Block::default()
+            .borders(Borders::ALL)
+            .title("User Information"),
+    );
+    frame.render_widget(user_info, container_user_info);
+
+    let [container_highlight, container_messages, container_input] = *Layout::default()
+        .direction(Direction::Vertical)
+        .constraints(
+            [
+                Constraint::Length(3),
+                Constraint::Min(1),
+                Constraint::Length(3),
+            ]
+            .as_ref(),
+        )
+        .split(middle) else {
+            panic!("The middle layout should have 3 chunks")
+        };
+
+    let text = Text::from(Line::from(vec![
+        "on ".into(),
+        "#room1".bold(),
+        " for ".into(),
+        "interesting talks about life".italic(),
+    ]));
+    let help_message = Paragraph::new(text).block(
+        Block::default()
+            .borders(Borders::ALL)
+            .title("Active Room Information"),
+    );
+    frame.render_widget(help_message, container_highlight);
+
+    let messages: Vec<ListItem> = app
+        .messages
+        .iter()
+        .enumerate()
+        .map(|(i, m)| {
+            let content = Line::from(Span::raw(format!("{i}: {m}")));
+            ListItem::new(content)
+        })
+        .collect();
+    let messages =
+        List::new(messages).block(Block::default().borders(Borders::ALL).title("Messages"));
+    frame.render_widget(messages, container_messages);
+
+    let input = Paragraph::new(app.input.as_str())
+        .style(match app.input_mode {
+            InputMode::Normal => Style::default(),
+            InputMode::Editing => Style::default().fg(Color::Yellow),
+        })
+        .block(Block::default().borders(Borders::ALL).title("Input"));
+    frame.render_widget(input, container_input);
+    match app.input_mode {
+        InputMode::Normal =>
+            // Hide the cursor. `Frame` does this by default, so we don't need to do anything here
+            {}
+
+        InputMode::Editing => {
+            // Make the cursor visible and ask ratatui to put it at the specified coordinates after
+            // rendering
+            frame.set_cursor(
+                // Draw the cursor at the current position in the input field.
+                // This position is can be controlled via the left and right arrow key
+                container_input.x + app.cursor_position as u16 + 1,
+                // Move one line down, from the border to the input line
+                container_input.y + 1,
+            )
+        }
+    }
+
+    let [container_room_users, container_usage] = *Layout::default()
+        .direction(Direction::Vertical)
+        .constraints(
+            [
+                Constraint::Min(1),
+                Constraint::Length(10),
+            ]
+            .as_ref(),
+        )
+        .split(right) else {
+            panic!("The left layout should have 2 chunks")
+        };
+
+    let room_users_list_items: Vec<ListItem> = vec!["jjohndoe", "jane", "john"]
+        .iter()
+        .map(|user_name| {
+            let content = Line::from(Span::raw(format!("@{user_name}")));
+
+            ListItem::new(content)
+        })
+        .collect();
+    let room_users_list = List::new(room_users_list_items)
+        .block(Block::default().borders(Borders::ALL).title("Room Users"));
+
+    frame.render_widget(room_users_list, container_room_users);
+
+    let mut usage_text = Text::from(vec![
+        Line::from(vec!["(q)".bold(), " to exit".into()]),
+        Line::from(vec!["(e)".bold(), " to start editing".into()]),
+    ]);
+    usage_text.patch_style(Style::default().add_modifier(Modifier::RAPID_BLINK));
+    let usage =
+        Paragraph::new(usage_text).block(Block::default().borders(Borders::ALL).title("Usage"));
+    frame.render_widget(usage, container_usage);
+}
